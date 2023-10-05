2cfe34fede2cbdd99e92ca79b2bf302f876ad5eb
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2023-10-06 01:01:23 +0530

    created the form to create the habbit and store it db

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+const expressLayout = require('express-ejs-layouts');
+
+app.use(express.urlencoded());
+app.use(expressLayout);
+app.use(express.static('./assets'))
+
+// to extract the style and js from different layouts
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true)
+
+// setting up the view engine
 app.set('view engine', 'ejs');
 app.set('views','./views')
 
+// use express router
+app.use('/', require("./routes"));
 
 app.listen(port,(err)=>{
     if(err){
