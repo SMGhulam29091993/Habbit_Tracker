fe3d1980204e4608049d3c3b9acba4fc6571db4f
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2023-10-05 23:36:00 +0530

    installed ejs and set up the view engine in main index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views','./views')
 
 
 app.listen(port,(err)=>{
@@ -11,3 +13,4 @@ app.listen(port,(err)=>{
     }
     console.log(`The server is up and running on port ${port}`);
 })
+
